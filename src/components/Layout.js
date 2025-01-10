4c54238879a952024d608398e5fff688bd774ddd
Dmytro <dimon.skorohodov@gmail.com>
2025-01-10 16:28:21 +0200

    push

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,15 +6,13 @@ import { useMediaQuery } from "@mui/material";
 import Footer from "./sections/footer/Footer";
 import MessengerComponent from "../components/UI/Messanger";
 import Cookies from "../components/sections/cookies/Cookies";
-import CustomCursor from "./UI/CustomCursor";
 
 export default function Layout() {
-  const isMobile = useMediaQuery("(max-width: 500px");
+  // const isMobile = useMediaQuery("(max-width: 500px");
   return (
     <>
       <I18nextProvider i18n={i18n}>
         {/* {!isMobile && <CustomCursor />} */}
-        <CustomCursor />
         <Header />
         <Outlet />
         <Footer />
